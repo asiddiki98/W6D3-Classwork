36dc210c7ad6d13dfe957bd40efef64a6e3627cb
soura934 <soura934@gmail.com>
2020-12-03 16:37:37 -0500

    doing association

--- first_routes/app/models/artwork_share.rb
+++ first_routes/app/models/artwork_share.rb
@@ -12,5 +12,15 @@ class ArtworkShare < ApplicationRecord
     validates :artwork_id, presence: true
     validates :viewer_id, presence: true
 
+    belongs_to :viewer,
+        primary_key: :id,
+        foreign_key: :viewer_id,
+        class_name: :User
+
+        belongs_to :artwork,
+            primary_key: :id,
+            foreign_key: :artwork_id,
+            class_name: :Artwork
+
     
 end
--- first_routes/db/migrate/20201203201325_create_artworks.rb
+++ first_routes/db/migrate/20201203201325_create_artworks.rb
@@ -1,9 +1,9 @@
 class CreateArtworks < ActiveRecord::Migration[5.2]
   def change
     create_table :artworks do |t|
-      t.string :title, null: false, unique: true
+      t.string :title, null: false
       t.string :image_url, null:false
-      t.integer :artist_id, null: false, unique: true
+      t.integer :artist_id, null: false
 
       t.timestamps
     end
